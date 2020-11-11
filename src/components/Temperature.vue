faa56c9605071ceffdb4d794b3839d20f4d18165
sunjlal <sunjaylal@gmail.com>
2020-11-10 21:03:38 -0600

    Getting started

---
+++ src/components/Temperature.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="q-pa-md">
+    <p>
+      Ideal Camping Temperature?
+    </p>
+
+    <p>
+      {{ standard.min }}&#176; F to {{ standard.max }}&#176; F
+    </p>
+
+    <q-range
+      v-model="standard"
+      :min="0"
+      :max="120"
+      :left-label-value="'Low of ' + standard.min + '° F'"
+      :right-label-value="'High of ' + standard.max + '° F'"
+      label
+      drag-range
+    />
+
+    <p>
+      (Temperature range of {{ standard.max - standard.min }}&#176; F)
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Temperature',
+  data () {
+    return {
+      standard: {
+        min: 50,
+        max: 90
+      }
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-page class="text-center q-pa-md question-form q-page doc-page">
+    <p>Hello Camper!</p>
+
+    <p>Where would you like to camp?</p>
+      <div class="q-my-md">
+        <q-input outlined v-model="city" label="City" />
+      </div>
+      or
+      <div class="q-my-md">
+        <q-input outlined v-model="zip" label="Zip" />
+      </div>
+    <p>When are you free to camp?</p>
+    <q-date v-model="days"
+    subtitle="Camping Window"
+    range
+    multiple />
+
+    <!-- <div class="q-pb-sm">
+      Model: {{ days }}
+    </div> -->
+
+    <Temperature> </Temperature>
+
+  </q-page>
+</template>
+
+<script>
+import Temperature from 'components/Temperature'
+
+export default {
+  name: 'PageIndex',
+  components: { Temperature },
+  data () {
+    return {
+      days: null,
+      city: null,
+      zip: null
+    }
+  }
+}
+</script>
+
+<style scoped>
+.question-form {
+  padding: 16px 46px;
+  font-weight: 300;
+  max-width: 900px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
